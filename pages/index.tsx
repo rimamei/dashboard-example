95b90ce6f036540e242574a7cba274cc3021936c
Rima <rimameih@gmail.com>
2022-09-18 23:11:15 +0700

    add button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import Container from "../components/Container";
 
 const Home: NextPage = () => {
   return (
     <Container title="Dashboard">
+      <button>
+        <Link href="/sign-in">Sign In</Link>
+      </button>
       <div>Dashboard</div>
     </Container>
   );
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { EmptyLayout } from '../layout/EmptyLayout'
+import Link from "next/link";
+import React from "react";
+import { EmptyLayout } from "../layout/EmptyLayout";
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div>Sign In No Default Layout</div>
+      <button style={{ width: "fit-content" }}>
+        <Link href="/">Dashboard</Link>
+      </button>
+    </div>
+  );
 }
 
-SignIn.Layout = EmptyLayout
+SignIn.Layout = EmptyLayout;
